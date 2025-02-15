43cd73ad0c75461f5ea26afd8344d444084b9ffd
amriteshx1 <amriteshx18@gmail.com>
2025-02-15 17:14:49 +0530

    Add multiple test cases

--- hash.js
+++ hash.js
@@ -102,3 +102,46 @@ class HashMap{
 }
 
 const test = new HashMap();
+
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+console.log(test.length());
+
+test.set('apple', 'green')
+test.set('banana', 'dark yellow')
+test.set('dog', 'black')
+console.log(test.length());
+
+test.set('moon', 'silver')
+console.log(test.length());
+
+console.log(test.get('apple'))
+console.log(test.get('banana')) 
+
+console.log(test.has('moon'))
+console.log(test.has('sun'))
+
+console.log(test.remove('carrot'))
+console.log(test.get('carrot')) 
+console.log(test.length())
+console.log(test.remove('sun'))
+
+console.log(test.keys()) 
+console.log(test.values()) 
+console.log(test.entries()) 
+
+test.clear()
+console.log(test.length())
+console.log(test.keys()) 
+console.log(test.values()) 
+console.log(test.entries())
